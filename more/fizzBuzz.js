8e65bb933043cc9a63e470755bcd9027ca5de6ee
ahaleymahaley <ahaleymahaley@gmail.com>
2025-07-07 22:00:22 +0300

    fizzBuzz solved

--- more/fizzBuzz.js
+++ more/fizzBuzz.js
@@ -1,5 +1,27 @@
 export const fizzBuzz = (str) => {
     let result = "";
-    // TODO
+    const range = str.split(",");
+    const begin = parseInt(range[0]);
+    const end = parseInt(range[1]);
+
+    for (let number = begin; number <= end; number++) {
+        if (number !== begin) {
+            result += ", ";
+        }
+        switch (true) {
+            case number % 15 === 0:
+                result += "FizzBuzz";
+                break;
+            case number % 5 === 0:
+                result += "Buzz";
+                break;
+            case number % 3 === 0:
+                result += "Fizz";
+                break;
+            default:
+                result += number;
+                break;
+        }
+    }
     return result;
-}
+};

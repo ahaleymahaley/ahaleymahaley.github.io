683babcb45943b1e91fbf9026103e700dd28386c
ahaleymahaley <ahaleymahaley@gmail.com>
2025-07-10 19:14:56 +0300

    perfectNumber

--- more/perfectNumber.js
+++ more/perfectNumber.js
@@ -1,5 +1,23 @@
 export const perfectNumber = (str) => {
-    let result = "";
-    // TODO
-    return result;
-}
+    let result = 0;
+    const number = parseInt(str);
+    let num = number;
+    let div = 2;
+    let map = new Map([[1, 1]]);
+    while (num != 1) {
+        if (num % div === 0) {
+            num /= div;
+            const mapCopy = new Map(map.entries());
+            mapCopy.forEach((element) => {
+                const mult = element * div;
+                if (mult < number) map.set(mult, mult);
+            });
+        } else {
+            div++;
+        }
+    }
+    map.forEach((element) => {
+        result += element;
+    });
+    return result === number;
+};

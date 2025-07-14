57cd7f001d011c9c85ba41e36b6b6c04fb2e55ad
ahaleymahaley <ahaleymahaley@gmail.com>
2025-07-14 08:59:35 +0300

    reverseInteger solved

--- more/reverseInteger.js
+++ more/reverseInteger.js
@@ -1,5 +1,15 @@
 export const reverseInteger = (str) => {
     let result = "";
-    // TODO
+    const number = parseInt(str);
+    let index = str.length - 1;
+    let end = 0;
+    if (number !== Math.abs(number)) {
+        result += "-";
+        end++;
+    }
+    while (index >= end) {
+        result += str[index];
+        index--;
+    }
     return result;
-}
+};

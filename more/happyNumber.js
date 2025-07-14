eac4f273b715b4d31d4c66d252d2cc570f15fbc0
ahaleymahaley <ahaleymahaley@gmail.com>
2025-07-14 08:27:28 +0300

    happyNumber solved

--- more/happyNumber.js
+++ more/happyNumber.js
@@ -1,5 +1,21 @@
 export const happyNumber = (str) => {
-    let result = "";
-    // TODO
+    let result = false;
+    let number = parseInt(str);
+    const sumOfSquareDigits = (number) => {
+        let sum = 0;
+        const numStr = number.toString();
+        for (let i = 0; i < numStr.length; i++) {
+            sum += Math.pow(parseInt(numStr[i]), 2);
+        }
+        return sum;
+    };
+    for (let i = 0; i < 10; i++) {
+        const sum = sumOfSquareDigits(number);
+        if (sum === 1) {
+            result = true;
+            break;
+        }
+        number = sum;
+    }
     return result;
-}
+};

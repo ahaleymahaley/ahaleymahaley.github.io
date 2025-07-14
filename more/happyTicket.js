e5b438ff8c5490c89528589552c1d411b5bc61a4
ahaleymahaley <ahaleymahaley@gmail.com>
2025-07-14 07:39:02 +0300

    happyTicket solved

--- more/happyTicket.js
+++ more/happyTicket.js
@@ -1,5 +1,10 @@
 export const happyTicket = (str) => {
-    let result = "";
-    // TODO
-    return result;
-}
+    let right = 0;
+    let left = 0;
+    const halfLen = str.length / 2;
+    for (let i = 0; i < halfLen; i++) {
+        left += parseInt(str[i]);
+        right += parseInt(str[i + halfLen]);
+    }
+    return left === right;
+};
